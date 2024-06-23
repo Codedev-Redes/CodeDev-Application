f416af592e099d65180b397e43d77a47b839eef6
unknown <renzoec.u@gmail.com>
2024-06-23 16:34:07 -0500

    feat: sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./student/student.module').then(m => m.StudentModule) } 
+  { path: '', loadChildren: () => import('./student/student.module').then(m => m.StudentModule) },
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) } 
 ];
 
 @NgModule({
